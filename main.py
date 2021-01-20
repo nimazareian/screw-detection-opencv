7c2de92a74f90d3d124b3da0076fa71bccb30def
Nima Zareian <nimazareian80@gmail.com>
2021-01-19 23:06:17 -0800

    Added rectangles around contours

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ cap = cv2.VideoCapture(0)
 cap.set(10,160)
 cap.set(3,1920) # Video Dimensions
 cap.set(4,1080)
+scale = 3
+widthPaper = 279 * scale #21.59cm
+heightPaper = 216 * scale #27.94cm
 
 cv2.namedWindow("TrackBars") #Names need to stay the same
 cv2.resizeWindow("TrackBars", 640, 240)
@@ -18,9 +21,18 @@ while True:
     if webcam: success, img = cap.read()
     else: img = cv2.imread(path)
 
+    # contours bigger than 1000, with 6 edges!?
+    img, contours = utils.getContours(img, showCanny=False, minArea=5000, filterEdges=4, draw=True)
+
+    if len(contours) != 0:
+        biggest = contours[0][2]
+        print(biggest)
+        imgWarp = utils.warpImg(img, biggest, widthPaper, heightPaper)
+        print("Shape", imgWarp.shape)
+        cv2.imshow("A5", imgWarp)
+
     thr1 = cv2.getTrackbarPos("Threshold 1","TrackBars")
     thr2 = cv2.getTrackbarPos("Threshold 2","TrackBars")
-    print(thr1,thr2)
     utils.getContours(img, cannyThr=[thr1,thr2], showCanny=True)
 
     img = cv2.resize(img, (0,0),None,1.5,1.5) # Scales after weve manipulated the photo
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import numpy as np
 def empty(val):
     pass
 
-def getContours(img, cannyThr=[200,200], showCanny=False):
+def getContours(img, cannyThr=[200,200], showCanny=False, minArea=1000, filterEdges = 0, draw=False):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (5,5),1)
     imgCanny = cv2.Canny(imgBlur, cannyThr[0], cannyThr[1])
@@ -13,3 +13,47 @@ def getContours(img, cannyThr=[200,200], showCanny=False):
     imgThre = cv2.erode(imgDial,kernel,iterations=2)
     if showCanny: cv2.imshow("canny", imgCanny)
 
+    contours, hiearchy = cv2.findContours(imgThre, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    finalContours = []
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > minArea:
+            peri = cv2.arcLength(i,True) # True for contour being closed
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True) # Approximation of edges of contour
+            bbox = cv2.boundingRect(approx) # Bounding box
+            print("bbox:", bbox)
+            if filterEdges > 0: #0  how many edges
+                if len(approx) == filterEdges:
+                    print("# of edges", len(approx))
+                    finalContours.append([len(approx), area, approx, bbox, i]) # Keep contour
+            else:
+                print("NOOOT FilterEdges # of edges")
+                # finalContours.append([len(approx), area, approx, bbox, i])
+
+    finalContours = sorted(finalContours, key = lambda x: x[1], reverse=True) #Sorting based on area (largest first)
+    if draw:
+        for contour in finalContours:
+            cv2.drawContours(img, contour[4], -1, (0,0,255), 3)
+
+    return img, finalContours
+
+def reorder(points):
+    print(points.shape)
+    pointsNew = np.zeros_like(points)
+    points = points.reshape(4,2)
+    add = points.sum(1)
+    pointsNew[0] = points[np.argmin(add)]
+    pointsNew[3] = points[np.argmax(add)]
+    diff = np.diff(points, axis=1)
+    pointsNew[1] = points[np.argmin(diff)]
+    pointsNew[2] = points[np.argmax(diff)]
+    return pointsNew
+
+def warpImg(img, points, width, height):
+    # print(points)
+    points = reorder(points)
+    pts1 = np.float32(points)
+    pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+    matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    imgWarp = cv2.warpPerspective(img, matrix, (width,height))
+    return imgWarp
